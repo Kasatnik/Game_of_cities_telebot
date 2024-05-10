a6a303dc9ae962876ca4a7f15654dfd06fdb2504
Roma <roman.kasatnik@gmail.com>
2024-05-10 21:38:58 +0300

    .

--- game_cities.py
+++ game_cities.py
@@ -10,7 +10,7 @@ import requests
 gc = geonamescache.GeonamesCache()
 cities = gc.get_cities()
 
-token = "6630004707:AAGbviWPwvbAl7oaAKD9WCOV-SdCG4oWjNE"
+token = "6530281381:AAE4bOrzoIYULEHWiF8tdf6R20oYez8v1OQ"
 images_api = "bAKzpGjWfG25uqAIVVt-NqfFbokGXxP3-DxQaVcAINk"
 bot = tel.TeleBot(token)
 creator = 6735254429
@@ -58,6 +58,7 @@ def user_message(message):
 
     written_user_cities.append(user_city)
     latest_letter = user_city[-1]
+    print(written_user_cities)
     if latest_letter in ["ь", "ъ", "ы"]:
         latest_letter = user_city[-2]
     written_bot_cities, lat, long = searching_cities(latest_letter)
@@ -67,8 +68,6 @@ def user_message(message):
     bot.send_location(message.from_user.id, lat, long)
     answerr = wiki(message, written_bot_cities)
     send_data(message, lat, long, answerr)
-    print(written_user_cities)
-
 
 def searching_cities(latest_letter):
     latest_letter = latest_letter.upper()
@@ -89,8 +88,11 @@ def user_location(message):
 
 def send_data(message, lat, long, answer):
     data = result(lat, long)
-    launch_url_img = url_img(user_city=data["data"]["state"])
     print(11111111, data)
+    if data["status"] == 'fail':
+        print("Status")
+        return
+    launch_url_img = url_img(user_city=data["data"]["state"])
     country = data["data"]["country"]
     tp = data["data"]["current"]["weather"]["tp"]
     aqius = data["data"]["current"]["pollution"]["aqius"]
@@ -112,12 +114,13 @@ def wiki(message, user_city):
 
     except wikipedia.exceptions.PageError:
         return "  "
+    except wikipedia.exceptions.DisambiguationError:
+        return "  "
 
 
 def url_img(user_city):
     url = f"https://api.unsplash.com/search/photos?page=1&query={user_city}&client_id={images_api}"
     response = requests.get(url)
-    print(response)
     if response.status_code == 200:
         json_data = response.json()
         results = json_data["results"]
@@ -131,6 +134,7 @@ def open_file_r(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
         return data
+
 
 
 def open_file_w(file_path, data):
